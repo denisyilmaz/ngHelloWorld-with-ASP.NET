54497c3ef07209b984107f4f5cf7d6225c479de9
Denis <Denis@DENIS-WINDOWS-P>
2017-06-22 16:21:26 +0200

    Updated Hello World detail view

--- src/HelloWorldForLexperten/HelloWorldForLexperten/Controllers/HelloWorldController.cs
+++ src/HelloWorldForLexperten/HelloWorldForLexperten/Controllers/HelloWorldController.cs
@@ -15,12 +15,18 @@ namespace HelloWorldForLexperten.Controllers
         }
         public ActionResult Detail()
         {
-            //if(DateTime.Today.DayOfWeek == DayOfWeek.Monday) 
-            //{
-            //    return new RedirectResult("/");
-            //};
-
-            //return Content("Hello World from the Hello World For Lexperten Controller!");
+            ViewBag.WelcomeMessage = "Hello World";
+            ViewBag.SeriesTitle = "The Amazing Spider-Man";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
+            ViewBag.Artists = new string[]
+            {
+                "Script: Dan Slott",
+                "Pencils: Humberto Ramos",
+                "Inks: Victor Olazaba",
+                "Colors: Edgar Delgado",
+                "Letters: Chris Eliopoulos"
+            };
 
             return View();
         }
